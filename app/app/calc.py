9c3b46fc11932f5937af752448e1122bc28813d0
Mohammed Zaheen <mohammed.zaheen@dbizsolution.com>
2023-11-21 12:34:45 +0530

    Configuring the postgresql database for the django app

---
+++ app/app/calc.py
@@ -0,0 +1,10 @@
+"""
+Calculator functions
+"""
+def add(num1, num2):
+    """ Adds two numbers and returns the result """
+    return num1 + num2
+
+def subtract(x ,y):
+    """ Subtract y from x and return the result"""
+    return x - y
---
+++ app/app/tests.py
@@ -0,0 +1,13 @@
+from django.test import SimpleTestCase
+from app import calc
+
+class CalcTests(SimpleTestCase):
+
+    def test_add(self):
+        res = calc.add(10,50)
+        self.assertEqual(res, 60)
+    
+    def test_subtract_numbers(self):
+        """ Test subtracting numbers"""
+        res = calc.subtract(10, 15)
+        self.assertEqual(res, -5)
