e294f3aebd97cebf683bc471c26d328df83a2de8
Mohammed Zaheen <mohammed.zaheen@dbizsolution.com>
2023-11-24 10:41:03 +0530

    Create user API created

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -33,3 +33,28 @@ class PublicUserApiTests(TestCase):
         user = get_user_model().objects.get(email=payload['email'])
         self.assertTrue(user.check_password(payload['password']))
         self.assertNotIn('password', res.data)
+
+    def test_user_with_email_exists_error(self):
+        """Test error returned if user with email exists."""
+        payload = {
+            'email': 'test@example.com',
+            'password': 'testpass123',
+            'name': 'Test 2'
+        }
+        create_user(**payload)
+        res = self.client.post(CREATE_USER_URL, payload)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_password_too_short_error(self):
+        """Test an error is returned if password is less than 5 chars."""
+        payload = {
+            'email': 'test@example.com',
+            'password': 'test',
+            'name': 'Test 2'
+        }
+        res = self.client.post(CREATE_USER_URL, payload)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+        user_exists = get_user_model().objects.filter(
+            email=payload['email']
+        ).exists()
+        self.assertFalse(user_exists)
