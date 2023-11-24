440b1af8367ed6387e1c7172d462671aa9541ea0
Mohammed Zaheen <mohammed.zaheen@dbizsolution.com>
2023-11-24 15:27:07 +0530

    Created tests for the manage user API.

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -132,4 +132,14 @@ class PrivateUserApiTests(TestCase):
 
         self.assertEqual(res.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
 
-    # def test_update_user_
+    def test_update_user_profile(self):
+        """Test updating the user profile for the authenticated user."""
+        payload = {'name': 'Updated name', 'password': 'newpassword123'}
+
+        res = self.client.patch(ME_URL, payload)
+
+        self.user.refresh_from_db()
+        self.assertEqual(self.user.name, payload['name'])
+        self.assertTrue(self.user.check_password(payload['password']))
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        
