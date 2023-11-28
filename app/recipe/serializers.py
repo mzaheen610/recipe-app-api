76b794016583d6463b4966b91df44f31c8459743
Mohammed Zaheen <mohammed.zaheen@dbizsolution.com>
2023-11-28 14:22:34 +0530

    Implemented update tags api

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -3,8 +3,10 @@ Serializers for recipe APIs
 """
 from rest_framework import serializers
 
-from core.models import Recipe
-
+from core.models import (
+    Recipe,
+    Tag,
+    )
 
 class RecipeSerializer(serializers.ModelSerializer):
     """serializer for recipes."""
@@ -19,3 +21,12 @@ class RecipeDetailSerializer(RecipeSerializer):
 
     class Meta(RecipeSerializer.Meta):
         fields = RecipeSerializer.Meta.fields + ['description']
+
+
+class TagSerializer(serializers.ModelSerializer):
+    """Serializer for the tags API."""
+
+    class Meta:
+        model = Tag
+        fields = ['id', 'name']
+        read_only_fields = ['id']
